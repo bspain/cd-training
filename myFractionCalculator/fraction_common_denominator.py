bd2c13bb5fcce61cfb8109004500a5ef5469ff7c
Benjamin Spain <2678462+bspain@users.noreply.github.com>
2025-01-26 08:35:43 -0600

    remove dead code

--- myFractionCalculator/fraction_common_denominator.py
+++
@@ -1,8 +0,0 @@
-import math
-
-class FractionCommonDenominator:
-    def calculate(self, denominator1, denominator2):
-        if denominator1 == denominator2:
-            return 1
-        else:
-            return math.lcm(denominator1, denominator2)
